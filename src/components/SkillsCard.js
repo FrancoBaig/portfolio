1bf50072677e9c263e42359680a266053ce56963
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-05-30 18:21:34 -0300

    skills card

--- src/components/SkillsCard.js
+++ src/components/SkillsCard.js
@@ -1,17 +1,44 @@
-import React from "react"
+import React from "react";
+import { Line } from "rc-progress";
+
+const skills = [
+    {
+        title: "React",
+        percent: 75
+    },
+    {
+        title: "Javascript",
+        percent: 80
+    },
+    {
+        title: "CSS",
+        percent: 85
+    },
+    {
+        title: "HTML",
+        percent: 85
+    }
+];
 
 function SkillsCards() {
     return (
         <div className="skills">
             <h4 className="skills__title">Front end</h4>
             <div className="skills__items">
-                <div className="skills__item">
-                    <p className="skills__text">React</p>
-                    <div className="progress-bar"></div>
-                </div>
+                {skills.map((elem) => (
+                    <div className="skills__item" key={elem.title}>
+                        <p className="skills__text">{elem.title}</p>
+                        <Line
+                            percent={elem.percent}
+                            strokeWidth={2}
+                            trailWidth={2}
+                            strokeColor="#2F80ED"
+                        />
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default SkillsCards
+export default SkillsCards;
