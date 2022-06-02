9705375137bff55021c8abc6fe15461be654e4c5
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-06-02 15:33:22 -0300

    email display

---
+++ src/components/Snackbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Snackbar() {}
+
+export default Snackbar;
--- src/components/SpeedDial.js
+++ src/components/SpeedDial.js
@@ -3,50 +3,53 @@ import React, { useState } from "react";
 function SpeedDial() {
     const [display, setDisplay] = useState(false);
 
-    const handleChatClose = () => {
-        setDisplay(false);
+    const toggleDisplay = () => {
+        setDisplay((prev) => !prev);
     };
 
-    const handleDisplay = () => {
-        setDisplay(true);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        toggleDisplay();
     };
+
     return (
         <>
             {display && (
-                <>
-                    <div className="backdrop" onClick={handleChatClose}></div>
-                    <div className="chat paper">
-                        <h3 className="chat__title">Contact me!</h3>
-                        <form className="chat__form">
-                            <label className="label" htmlFor="email" required>
-                                Your email:
-                            </label>
-                            <input
-                                className="input"
-                                type="email"
-                                name="email"
-                            />
-                            <label className="label" htmlFor="input">
-                                Send me a message:
-                            </label>
-                            <textarea
-                                className="textarea"
-                                name="input"
-                                rows="21"
-                                cols="50"
-                                required
-                            />
-                            <button
-                                className="btn btn-lg btn--blue"
-                                type="submit"
-                            >
-                                Send
-                            </button>
-                        </form>
-                    </div>
-                </>
+                <div className="backdrop" onClick={toggleDisplay}></div>
             )}
-            <div className="speed-dial" onClick={handleDisplay}>
+            <div
+                className={`chat paper ${display ? "chat--active" : ""}
+            `}
+            >
+                <h3 className="chat__title">Contact me!</h3>
+                <form className="chat__form">
+                    <label className="label" htmlFor="email" required>
+                        Your email:
+                    </label>
+                    <input className="input" type="email" name="email" />
+                    <label className="label" htmlFor="input">
+                        Send me a message:
+                    </label>
+                    <textarea
+                        className="textarea"
+                        name="input"
+                        rows="18"
+                        cols="50"
+                        placeholder="Send me a message"
+                        required
+                    />
+                    <button
+                        className={`btn btn-lg btn--blue ${
+                            display ? "" : "btn--hidden"
+                        }`}
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
+                        Send
+                    </button>
+                </form>
+            </div>
+            <div className="speed-dial" onClick={(e) => toggleDisplay(e)}>
                 <i className="icon fa-solid fa-envelope"></i>
             </div>
         </>
